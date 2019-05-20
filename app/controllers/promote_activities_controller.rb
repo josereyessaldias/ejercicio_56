80e6b8c4768ac2e4e3407d3edd25219328faaf14
josereyessaldias <jpreyess@uc.cl>
2019-05-19 23:38:13 -0400

    reescritura de rutas casi lista

--- app/controllers/promote_activities_controller.rb
+++ app/controllers/promote_activities_controller.rb
@@ -1,9 +1,9 @@
 class PromoteActivitiesController < ApplicationController
   authorize_resource
 
-  def index
-  	@user = User.find(params[:user_id])
-  	@promotes = PromoteActivity.where(user_id: params[:user_id])
+  def show
+  	@user = User.find(params[:id])
+  	@promotes = PromoteActivity.where(user_id: params[:id])
   end
 
   def new
@@ -13,17 +13,17 @@ class PromoteActivitiesController < ApplicationController
 
   def create
     @promote_activity = PromoteActivity.new
-    @promote_activity.user_id = params[:user_id]
+    @promote_activity.user_id = current_user.id
     @promote_activity.activity_id = params[:promote_activity][:activity_id]
     @promote_activity.amount = params[:promote_activity][:amount]
     @promote_activity.save
-    redirect_to promote_activities_path(current_user)
+    redirect_to promote_activity_path(current_user.id)
   end
 
   def destroy
     @promote_activity = PromoteActivity.find(params[:id])
     @promote_activity.destroy
-    redirect_to promote_activities_path(current_user), notice: 'La promoción fue eliminada'
+    redirect_to promote_activity_path(current_user.id), notice: 'La promoción fue eliminada'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,15 @@ Rails.application.routes.draw do
   resources :activities, only: [:create, :new, :show, :edit, :update]
   resources :user_activities, only: [:create, :edit, :update, :destroy]
   resources :contacts, only: [:create, :destroy]
-  
+  resources :promote_activities, only: [:new, :create, :show, :destroy]
 
-  get 'promote_activities/index'
+
   get 'pages/index'
   get 'pages/politica'
 
   get 'user_calendar/:user_id', to: 'pages#user_calendar', as: 'user_calendar'
   patch 'edit_calendar/:activity_id', to: 'user_activities#update_calendar', as: 'update_user_calendar'
   
-  get 'promote_activities/:user_id', to: 'promote_activities#index', as: 'promote_activities'
-  get 'new_promote', to: 'promote_activities#new', as: 'new_promote'
-  post 'create_promote/:user_id', to: 'promote_activities#create', as: 'create_promote'
-  delete 'promote/:id', to: 'promote_activities#destroy', as: 'destroy_promote'
-
  
 
   resources :billings, only: [:index] do
